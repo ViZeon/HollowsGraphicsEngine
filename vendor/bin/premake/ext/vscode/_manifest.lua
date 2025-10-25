95aad4f2be1a7b06a4dc5756962a3262a2b27123
zieviox <zieviox@gmail.com>
2025-10-25 09:41:06 -0400

    removed useless clutter

--- vendor/bin/premake/ext/vscode/_manifest.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "init.lua",
-    "_preload.lua",
-    "vscode.lua",
-    "vscode_workspace.lua",
-    "vscode_project.lua",
-    "vscode_tasks.lua",
-    "vscode_launch.lua"
-}
--- vendor/bin/premake/ext/vscode/init.lua
+++
@@ -1,5 +0,0 @@
-local p = premake
-
-for _, file in ipairs(dofile("_manifest.lua")) do
-    include(file)
-end
