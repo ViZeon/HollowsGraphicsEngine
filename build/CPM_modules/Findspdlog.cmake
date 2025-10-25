4078b13133ab757ba11ae8951df543ae624d6386
zieviox <zieviox@gmail.com>
2025-10-25 09:25:10 -0400

    yeah... forgot to stage again

---
+++ build/CPM_modules/Findspdlog.cmake
@@ -0,0 +1,3 @@
+include("D:/Work/Game Engine/The Hollows/HollowsGraphicsEngine/build/cmake/CPM.cmake")
+CPMAddPackage("GITHUB_REPOSITORY;gabime/spdlog;VERSION;1.14.1;EXCLUDE_FROM_ALL;YES;SYSTEM;YES;")
+set(spdlog_FOUND TRUE)
