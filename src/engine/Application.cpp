2263e4a3e4eb63e7a346d5bb077dd98824718226
Vi-Zeon <6zeon9@gmail.com>
2025-11-10 12:00:37 -0800

    dunno

--- src/engine/Application.cpp
+++ src/engine/Application.cpp
@@ -1,6 +1,11 @@
 #include <Application.h>
-#include <SDL3/SDL.h>
+
 //#include <diligent>
+
+using namespace Diligent;
+
+//RefCntAutoPtr<IEngineFactory> pFactory;
+
 
 namespace Hollows {
     Application::Application() {
@@ -12,12 +17,52 @@ namespace Hollows {
 
 
 
+enum class NativeBackend {
+    Win32,
+    Cocoa,
+    X11,
+    Wayland,
+    Unknown
+};
+
+struct NativeWindow {
+    void* handle = nullptr;
+    NativeBackend backend = NativeBackend::Unknown;
+};
+
+inline NativeWindow GetNativeWindow(SDL_Window* window)
+{
+    SDL_PropertiesID props = SDL_GetWindowProperties(window);
+    NativeWindow out;
+
+    const struct {
+        const char* key;
+        NativeBackend backend;
+    } keys[] = {
+        {"SDL_PROP_WINDOW_WIN32_HWND_POINTER",   NativeBackend::Win32},
+        {"SDL_PROP_WINDOW_COCOA_WINDOW_POINTER", NativeBackend::Cocoa},
+        {"SDL_PROP_WINDOW_X11_WINDOW_POINTER",   NativeBackend::X11},
+        {"SDL_PROP_WINDOW_WAYLAND_SURFACE_POINTER", NativeBackend::Wayland}
+    };
+
+    for (auto& k : keys) {
+        void* h = SDL_GetPointerProperty(props, k.key, nullptr);
+        if (h) { out.handle = h; out.backend = k.backend; break; }
+    }
+
+    return out;
+}
+
 
 void window() {
     SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window* window = SDL_CreateWindow("SDL3 is ON, Bitches",
         800, 600, SDL_WINDOW_RESIZABLE);
+
+	SDL_PropertiesID props = SDL_GetWindowProperties(window);
+
+	//void* hwind_void = SDL_GetPointerProperty(props, "SDL_PROP_WINDOW");
 
     bool running = true;
     SDL_Event e;
@@ -32,6 +77,11 @@ void window() {
     SDL_Quit();
 }
 
+
+
+
+
+//THE  FUNCTION THAT MATTERS
     void Application::Run() {
         window();
 /*        while (true) {
--- src/engine/Core.h
+++ src/engine/Core.h
@@ -1,5 +1,13 @@
 #pragma once
 
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_properties.h>
+
+
+#include <EngineFactory.h>
+//#include <diligentcore/graphics/GraphicsEngine/interface/engineFactoryVk.h>
+//#include <diligentcore/graphics/GraphicsEngine/interface/engineFactoryOpenGL.h>
+
 //#ifdef HZ_PLATFORM_WINDOWS
     #ifdef HZ_BUILD_DLL
         #define HOLLOWS_API __declspec(dllexport)
